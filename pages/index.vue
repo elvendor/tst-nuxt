a921405d43a4db064a6c82d11edbfa020c34a3bf
elvendor <bayram4ik@gmail.com>
2021-12-28 23:26:03 +0300

    ok

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtLink to="/tours/1">TOURS CATALOG</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/tour/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ data.name }}</h1>
+    <img :src="data.images[0].high" />
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData(context) {
+      const { data } = await context.app.$axios.get(
+        `https://api.travelshopbooking.com/b2c/tours/${context.params.slug}`
+      )
+      return { data }
+    }
+  }
+</script>
